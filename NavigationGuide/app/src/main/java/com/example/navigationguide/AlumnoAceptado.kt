1b3ee8971535f2cb67702f31efb1a1bf890ffcff
Eduardoaot <Eduardo.A.O.T@outlook.com>
2025-07-16 21:16:56 -0600

    Primer cambio eduardo es un chingos

--- NavigationGuide/app/src/main/java/com/example/navigationguide/AlumnoAceptado.kt
+++ NavigationGuide/app/src/main/java/com/example/navigationguide/AlumnoAceptado.kt
@@ -26,6 +26,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
+//Eduardo es un chingon
 @Composable
 fun AlertaAlumnoAceptado(onDismiss: () -> Unit){
     Box(
